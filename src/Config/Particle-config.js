22eff56f03c9e7bd0408d6f3432e52a52b0ab52e
Y4KI <yaki07gaming@gmail.com>
2021-09-21 12:06:49 +0500

    Particles style changed

--- src/Config/Particle-config.js
+++ src/Config/Particle-config.js
@@ -3,7 +3,7 @@ const ParticleConfig = {
     number: {
       value: 100,
       density: {
-        enable: true,
+        enable: false,
         value_area: 700,
       },
     },
@@ -19,18 +19,13 @@ const ParticleConfig = {
       polygon: {
         nb_sides: 3,
       },
-      image: {
-        src: "img/github.svg",
-        width: 100,
-        height: 100,
-      },
     },
     opacity: {
       value: 0.5,
       random: false,
       anim: {
         enable: false,
-        speed: 1,
+        speed: 2,
         opacity_min: 0.1,
         sync: false,
       },
@@ -54,7 +49,7 @@ const ParticleConfig = {
     },
     move: {
       enable: true,
-      speed: 2,
+      speed: 1,
       direction: "none",
       random: false,
       straight: false,
@@ -68,42 +63,17 @@ const ParticleConfig = {
     },
   },
   interactivity: {
-    detect_on: "canvas",
+    detect_on: "window",
     events: {
       onhover: {
         enable: false,
         mode: "repulse",
       },
       onclick: {
-        enable: true,
+        enable: false,
         mode: "push",
       },
       resize: true,
-    },
-    modes: {
-      grab: {
-        distance: 400,
-        line_linked: {
-          opacity: 1,
-        },
-      },
-      bubble: {
-        distance: 400,
-        size: 40,
-        duration: 2,
-        opacity: 8,
-        speed: 3,
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.4,
-      },
-      push: {
-        particles_nb: 4,
-      },
-      remove: {
-        particles_nb: 2,
-      },
     },
   },
   retina_detect: true,
