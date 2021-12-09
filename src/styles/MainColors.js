1b5121c107e4e896cd642d4fd5e2760f7f356c4c
Y4KI <yaki07gaming@gmail.com>
2021-12-09 13:38:27 +0500

    Colors was changed

--- src/styles/MainColors.js
+++ src/styles/MainColors.js
@@ -1,8 +1,8 @@
 export const MainColors = {
-  dark_1: "#29102e",
-  dark_2: "#34345c",
-  orange_1: "#a64a53",
-  orange_2: "#d87a55",
-  yellow: "#f5b657",
-  blue: "#826488",
+  dark_1: "#041C32", // BGCOLOR
+  dark_2: "#ECB365", //
+  orange_1: "#064663", //
+  orange_2: "#F0BB62", //
+  yellow: "#DD4A48", //
+  blue: "#064663", //
 };
